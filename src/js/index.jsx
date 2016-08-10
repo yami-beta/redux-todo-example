dd5838a32861cf074b4ff70f6596152132b12120
yami_beta <yami-beta@users.noreply.github.com>
2016-08-10 16:22:25 +0900

    Add action and reducer

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import todo from './reducers/todos';
+import { addTodo } from './actions';
 import App from './components/App';
 
+let store = createStore(todo);
+store.dispatch(addTodo('Hello World!'));
+console.log(store.getState());
+
 render(
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
---
+++ src/js/reducers/todos.js
@@ -0,0 +1,12 @@
+const todo = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+        id: action.id,
+        text: action.text,
+      };
+    default:
+      return state;
+  }
+};
+export default todo;
