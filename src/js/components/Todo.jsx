732e8a0abdb8a27c2ee6a434f9899cd33d47cda6
yami_beta <yami-beta@users.noreply.github.com>
2016-08-16 15:05:44 +0900

    Add completed prop

--- src/js/components/Todo.jsx
+++ src/js/components/Todo.jsx
@@ -1,13 +1,14 @@
 import React, { PropTypes } from 'react';
 
-const Todo = ({ text }) => (
-  <li>
+const Todo = ({ text, completed }) => (
+  <li style={{ textDecoration: completed ? 'line-through' : 'none' }}>
     {text}
   </li>
 );
 
 Todo.propTypes = {
   text: PropTypes.string.isRequired,
+  completed: PropTypes.bool.isRequired,
 };
 
 export default Todo;
--- src/js/reducers/todos.js
+++ src/js/reducers/todos.js
@@ -4,6 +4,7 @@ const todo = (state, action) => {
       return {
         id: action.id,
         text: action.text,
+        completed: false,
       };
     default:
       return state;
